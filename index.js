77910b3ad2b2daaccb4dfd6f29688e36201d6708
Kyle Fossman <kjfossman@gmail.com>
2021-01-19 16:59:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // add solution here
+let bees = []
+
+function theBeatlesPlay(musicians, instruments){
+  for(let i = 0; i < musicians.length; i++){
+  bees.push(`${musicians[i]} plays ${instruments[i]}`)
+}
+return bees
+}
+
+let n = 0
+let newFacts = []
+function johnLennonFacts(array){
+  while(array.length > n){
+    newFacts.push(`${array[n]}!!!`)
+    n++
+  }
+  return newFacts
+}
+
+
+let t = 14
+function incrementVaraible(){
+  return t--
+}
+
+function iLoveTheBeatles(num){
+  var empty =[]
+  do{
+    empty.push(`I love the Beatles!`);
+} while(incrementVaraible() > num);
+return empty
+}
